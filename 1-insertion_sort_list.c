4b1afa2fba0bffd42b70981e760a1a7c33c15714
Moha-A1 <11469@holbertonstudents.com>
2025-08-06 16:03:23 +0200

    task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Trie une liste doublement chaînée d'entiers
+ *                       en utilisant l'algorithme de tri par insertion
+ * @list: Pointeur vers la tête de la liste doublement chaînée
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *temp, *prev;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	current = (*list)->next;
+
+	while (current != NULL)
+	{
+		temp = current;
+		prev = current->prev;
+
+		while (prev != NULL && prev->n > temp->n)
+		{
+			if (prev->prev != NULL)
+				prev->prev->next = temp;
+			else
+				*list = temp;
+
+			if (temp->next != NULL)
+				temp->next->prev = prev;
+
+			prev->next = temp->next;
+			temp->prev = prev->prev;
+			temp->next = prev;
+			prev->prev = temp;
+
+
+			prev = temp->prev;
+
+
+			print_list(*list);
+		}
+
+		current = current->next;
+	}
+}
--- sort.h
+++ sort.h
@@ -1,5 +1,8 @@
 #ifndef SORT
 #define SORT
+
+#include <stddef.h>
+#include <stdlib.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -19,5 +22,6 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void print_list(const listint_t *list);
 void selection_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 #endif
